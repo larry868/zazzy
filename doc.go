bf01a24c2ad39cde9d01000b3e777f73776ba8fc
lolorenzo777 <lolorenzo777@sunraylab.net>
2022-05-03 10:43:54 +0200

    init doc

---
+++ doc.go
@@ -0,0 +1,10 @@
+/*
+Zazzy is a static site generator. 
+
+Basics
+
+Zazzy generate website files by copying and converting files of a given direcory. `markdown` files are converted into html files using a layout tamplate. html files and markdown files can make use of variables with placaholders.
+
+
+*/
+package main
